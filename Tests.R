ebcf3332d9cda63e81618f23fde12bf2c0456056
MarkPlatts <mdplatts@yahoo.co.uk>
2016-12-14 11:43:23 +0000

    Changed all test so that they output what is being tested regardless of whether passes or fails

--- Tests.R
+++ Tests.R
@@ -6,23 +6,23 @@ print(Sys.time())
 #Test
 Test__Check_Average_Quota_across_models_4_AdultCod_Fleet1_HighestValue = function()
 {
+  print("Test__Check_Average_Quota_across_models_4_AdultCod_Fleet1_HighestValue")
+  
   #Input data:
   Group_Cod = "Cod (adult)"
   Fleet_DemersalTrawl = "FleetNo1"
   StrategyType_Highest_Value = "Highest value"
-  Test_Path = "C:/Users/Mark/Dropbox/GAP2_MSE Plugin2/R Code/TestFolder_R_Plotting_MSE_Plugin/withBiomassForcing_Yearly_Results HCR type1 and 3/Results/"
+  Test_Path = GetResultsLocation()
   
   #Test:
   Cod_adult_Average_Across_Quota = Average_Quota_Across_Models_And_RegTypes(Group_Cod,Fleet_DemersalTrawl,StrategyType_Highest_Value,Test_Path)
   Correct = c(0.0562363408,	0.0691622814,	0.0740509911,	0.0683924373,	0.0587781737,	0.0484810997,	0.0413565884,	0.0390158918,	0.0417923933,	0.0486157672,	0.0489791314,	0.0408512426,	0.0334640422,	0.028770516,	0.0250194448,	0.0224817797,	0.0219612043,	0.021143502,	0.0180477091,	0.0148551648)
   
   #Outcome
-  if(sum(Cod_adult_Average_Across_Quota!=Correct)>0){
-    print("Test__Check_Average_Quota_across_models_4_AdultCod_Fleet1_HighestValue FAILED")
-    #print(paste("Cod_adult_Average_Across_Quota = ", Cod_adult_Average_Across_Quota))
-    #print(paste("Correct = ", Correct))
+  if(isTRUE(all.equal(Cod_adult_Average_Across_Quota, Correct, tolerance = 1e-8))){
+    print("PASSED")
   } else{
-    print("Test__Check_Average_Quota_across_models_4_AdultCod_Fleet1_HighestValue PASSED")
+    print("FAILED")
   }
 }
 
@@ -30,25 +30,30 @@ Test__Check_Average_Quota_across_models_4_AdultCod_Fleet1_HighestValue = functio
 #Test
 Test__LoadFile_ContainsListStrings = function()
 {
+  print("Test__LoadFile_ContainsListStrings")
+  
   #Input data:
   Group_Skate_and_Ray = "Targ_Skate + cuckoo ray"
   Fleet_GearsUsingHooks = "FleetNo7"
-  Test_Path = "C:/Users/Mark/Dropbox/GAP2_MSE Plugin2/R Code/TestFolder_R_Plotting_MSE_Plugin/withBiomassForcing_Yearly_Results HCR type1 and 3/Results/HCRQuota_Targ/"
+  Test_Path = paste(GetResultsLocation(),"HCRQuota_Targ/",sep="")
   
   #Test:
   ResultFile = LoadFile_ContainsListStrings(Dir.Path = Test_Path, StringsInFileName = c(Group_Skate_and_Ray,Fleet_GearsUsingHooks))
-  CorrectFile = read.csv("C:/Users/Mark/Dropbox/GAP2_MSE Plugin2/R Code/TestFolder_R_Plotting_MSE_Plugin/withBiomassForcing_Yearly_Results HCR type1 and 3/Results/HCRQuota_Targ/HCR_Quota_Targ_Skate + cuckoo ray_GroupNo12_FleetNo7.csv",skip=7, head=T)
+  CorrectFile = read.csv(paste(GetResultsLocation(), "HCRQuota_Targ/HCR_Quota_Targ_Skate + cuckoo ray_GroupNo12_FleetNo7.csv",sep=""),skip=7, head=T)
+ 
   #Outcome
-  if(ResultFile!=CorrectFile){
-    print("Test__LoadFile_ContainsListStrings FAILED")
-  } else{
-    print("Test__LoadFile_ContainsListStrings PASSED")
+  if(all.equal(ResultFile,CorrectFile)){
+    print("PASSED")
+  } else {
+    print("FAILED")
   }
 }
 
 #Test
 Test__StringContains_StringDoesNotExist = function()
 {
+  print("Test__StringContains_StringDoesNotExist")
+  
   #input:
   FileName = "HCR_Quota_Targ_Benthic microflora (incl Bacteria protozoa))_GroupNo63_FleetNo3"
   String2test = "Hello"
@@ -58,10 +63,10 @@ Test__StringContains_StringDoesNotExist = function()
   
   #Results
   if (Outcome==TRUE){
-    print("Test__StringContains_StringDoesNotExist FAILED!")
+    print("FAILED!")
   }
   if (Outcome==FALSE){
-    print("Test__StringContains_StringDoesNotExist PASSED!")
+    print("PASSED!")
   }
   
 }
@@ -69,6 +74,8 @@ Test__StringContains_StringDoesNotExist = function()
 
 Test__StringContains_StringDoesExist = function()
 {
+  print("Test__StringContains_StringDoesExist")
+  
   #Input:
   FileName = "HCR_Quota_Targ_Benthic microflora (incl Bacteria protozoa))_GroupNo63_FleetNo3"
   String2test = "Benthic microflora (incl Bacteria protozoa)"
@@ -78,15 +85,17 @@ Test__StringContains_StringDoesExist = function()
   
   #Result:
   if (Outcome==TRUE){
-    print("Test__StringContains_StringDoesNotExist PASSED!")
+    print("PASSED!")
   }
   if (Outcome==FALSE){
-    print("Test__StringContains_StringDoesNotExist FAILED!")
+    print("FAILED!")
   }
 }
 
 Test__StringContains_AllStrings_StringsDoExist_FindsThem = function()
 {
+  print("Test__StringContains_AllStrings_StringsDoExist_Finds_Them") 
+  
   #Input
   MultipleStringsVector = c("Hello", "Goodbye")
   String_Containing_String1_and_String2 = "Hello John, its very nice to meet you, Sorry I cant stop. Goodbye John"
@@ -97,15 +106,17 @@ Test__StringContains_AllStrings_StringsDoExist_FindsThem = function()
     
   #Result:
   if(Outcome == FALSE){
-    print("Test__StringContains_AllStrings_StringsDoExist_Finds_Them FAILED!")
+    print("FAILED!")
   }
   if(Outcome == TRUE){
-    print("Test__StringContains_AllStrings_StringsDoExist_Finds_Them PASSED!")
+    print("PASSED!")
   }
 }
 
 Test__StringContains_AllStrings_StringsDontExist_DoesNotFindThem = function()
 {
+  print("Test__StringContains_AllStrings_StringsDontExist_DoesNotFindThem")
+  
   #Input
   MultipleStringsVector = c("Hello", "Goodbye")
   String_Containing_String1_and_String2 = "Hello John, its very nice to meet you, Sorry I cant stop. John"
@@ -115,17 +126,17 @@ Test__StringContains_AllStrings_StringsDontExist_DoesNotFindThem = function()
                                       MultipleStrings2Check = MultipleStringsVector)
   #Result:
   if(Outcome == TRUE){
-    print("Test__StringContains_AllStrings_StringsDontExist_DoesNotFindThem FAILED!")
+    print("FAILED!")
   }
   if(Outcome == FALSE){
-    print("Test__StringContains_AllStrings_StringsDontExist_DoesNotFindThem PASSED!")
+    print("PASSED!")
   }
 }
 
 
 Test__SubsetVectorStrings_ContainingString__Finds_All_Highest_Value = function()
 {
-
+  print("Test__SubsetVectorStrings_ContainingString__Finds_All_Highest_Value")
   #Input
   UniqueStrategies = Load_Strategies("../TestInput/UniqueStrategies.csv")
   Regulation_HighestValue = "Highest value"
@@ -135,16 +146,17 @@ Test__SubsetVectorStrings_ContainingString__Finds_All_Highest_Value = function()
   Outcome = SubsetVectorStrings_ContainingString(UniqueStrategies, Regulation_HighestValue)
 
   #Result:
-  if(All_Vector_Indices_Identical(Outcome, CorrectStrategies)){
-    print("Test__SubsetVectorStrings_ContainingString__Finds_All_Highest_Value PASSED!")
+  if(all.equal(Outcome, CorrectStrategies)){
+    print("PASSED!")
   } else {
-    print("Test__SubsetVectorStrings_ContainingString__Finds_All_Highest_Value FAILED!")
+    print("FAILED!")
   }
 }
 
 
 Test__LoadUniqueStrategies__SameUniqueFile = function()
 {
+  print("Test__LoadUniqueStrategies__SameUniqueFile")
   
   #Input
   Outcome = LoadUniqueStrategies(GetResultsLocation())
@@ -152,10 +164,10 @@ Test__LoadUniqueStrategies__SameUniqueFile = function()
   #Test:
   CorrectStrategies = as.vector(Load_Strategies("../TestInput/UniqueStrategies.csv"))
   #Result:
-  if(All_Vector_Indices_Identical(Outcome, CorrectStrategies)){
-    print("Test__LoadUniqueStrategies__SameUniqueFile PASSED!")
+  if(all.equal(Outcome, CorrectStrategies)){
+    print("PASSED!")
   } else {
-    print("Test__LoadUniqueStrategies__SameUniqueFile FAILED!")
+    print("FAILED!")
   }
 }
 
@@ -182,20 +194,13 @@ GetResultsLocation = function()
   return("C:/Users/Mark/Dropbox/GAP2_MSE Plugin2/R Code/TestFolder_R_Plotting_MSE_Plugin/withBiomassForcing_Yearly_Results HCR type1 and 3/Results/")
 }
 
-#Check all indices of a vector are identical
-All_Vector_Indices_Identical = function(vec1, vec2)
-{
-  if(is.null(vec1) | is.null(vec2)) return (FALSE)
-  return(sum(vec1!=vec2)==0)
-}
-
 
 # Run Tests ---------------------------------------------------------------
+Test__Check_Average_Quota_across_models_4_AdultCod_Fleet1_HighestValue()
 Test__LoadUniqueStrategies__SameUniqueFile()
 Test__SubsetVectorStrings_ContainingString__Finds_All_Highest_Value()
 Test__StringContains_AllStrings_StringsDontExist_DoesNotFindThem()
 Test__StringContains_AllStrings_StringsDoExist_FindsThem()
-#Test__Check_Average_Quota_across_models_4_AdultCod_Fleet1_HighestValue()
 Test__LoadFile_ContainsListStrings()
 Test__StringContains_StringDoesNotExist()
 Test__StringContains_StringDoesExist()
