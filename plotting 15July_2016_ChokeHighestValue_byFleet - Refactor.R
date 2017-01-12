5081570df9b8cf661a294535505766ae0face9e6
MarkPlatts <mdplatts@yahoo.co.uk>
2017-01-12 14:03:31 +0000

    Improved structure of code that runs for plotting trajectories

--- plotting 15July_2016_ChokeHighestValue_byFleet - Refactor.R
+++ plotting 15July_2016_ChokeHighestValue_byFleet - Refactor.R
@@ -1,5 +1,7 @@
 plot_type = function(type2plot) {
   
+  options(scipen = 10)
+  
   setwd("C:/Users/Mark/Desktop/MSE_Plugin_Results_Plotting/")
   source("initialisation.R")
   source("B_Trajectories.R")
@@ -16,90 +18,123 @@ plot_type = function(type2plot) {
 
   create.plot.dirs(params)
   
+  ###HCR F Cons trajectories
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
   if(type2plot==1){
     params$MORT_HCRF_Cons <- T;  if(params$MORT_HCRF_Cons) {setwd("HCRF_Cons");params$YLAB<-"F";print("Plotting MORT_HCRF_Cons")} # what are these?
+    plot_fishing_trajectories(params)
   }
+  
+  ###HCR F Targ trajectories
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
   if(type2plot==2){
     params$MORT_HCRF_Targ<- T;   if(params$MORT_HCRF_Targ) {setwd("HCRF_Targ");params$YLAB<-"F";print("Plotting HCRF_Targ")} # what are these?
+    plot_fishing_trajectories(params)
   }
+  
+  ###HCR Quota Cons trajectories
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
   if(type2plot==3){
     params$QUOTA_HCRF_Cons<- T;  if(params$QUOTA_HCRF_Cons) {setwd("HCRQuota_Cons");params$YLAB<-"Quota (t)";print("Plotting HCRQuota_Cons")} # what are these?
-  }
+    plot_fishing_trajectories(params)
+  }  
+  
+  ###HCR Quota Targ trajectories
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
   if(type2plot==4){
     params$QUOTA_HCRF_Targ<- T;  if(params$QUOTA_HCRF_Targ) {setwd("HCRQuota_Targ");params$YLAB<-"Quota (t)";print("Plotting HCRQuota_Targ")} # what are these?
+    plot_fishing_trajectories(params)
   }
+  
+  ###Real Landed F trajectories
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
   if(type2plot==5){
     params$MORT_REAL_LandF <- T; if(params$MORT_REAL_LandF) {setwd("RealisedLandedF");params$YLAB<-"F";print("Plotting MORT_REAL_LandF")}
+    plot_fishing_trajectories(params)
   }
+  
+  ###Real Discard F trajectories
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
   if(type2plot==6){
     params$MORT_REAL_DiscF <- T; if(params$MORT_REAL_DiscF) {setwd("RealisedDiscardedF");params$YLAB<-"F";print("Plotting MORT_REAL_DiscF")}
+    plot_fishing_trajectories(params)
   }
+  
+  ###Real F trajectories
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
   if(type2plot==7){
     params$MORT_REAL_F <- T;     if(params$MORT_REAL_F) {setwd("RealisedF");params$YLAB<-"F";print("Plotting MORT_REAL_F")}
+    plot_fishing_trajectories(params)
   }
+  
+  ###Catch trajectories
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
   if(type2plot==8){
     params$CATCH <-T;    if(params$CATCH) {setwd("CatchTrajectories"); params$YLAB<-"Catch (t/year)";print("Plotting CATCH")}
+    plot_fishing_trajectories(params)
   }
+  
+  ###Landed trajectories
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
   if(type2plot==9){
     params$LANDING <- T; if(params$LANDING) {setwd("LandingsTrajectories"); params$YLAB<-"Landings (t/year)";print("Plotting LANDING")}
+    plot_fishing_trajectories(params)
   }
+  
+  ###Discard trajectories
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
   if(type2plot==10){
     params$DISCARD <- T; if(params$DISCARD) {setwd("DiscardsTrajectories"); params$YLAB<-"Discards (t/year)";print("Plotting DISCARD")}
+    plot_fishing_trajectories(params)
   }
   
+  ###Effort trajectories
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
   if(type2plot==11){
     params$EFFORT <-T;print("Plotting EFFORT")
+    plot_effort_trajectories(params)
   }
-  if(type2plot==12){
-    params$VALUE <- T;print("Plotting VALUE")
-  }
-  if(type2plot==13){
-    params$BIOMASS <-T;print("Plotting BIOMASS")
-  }
-  
-  if(type2plot==14){
-    params$HIGHEST_VALUE <- T;print("Plotting HIGHEST_VALUE")
-  }
-  if(type2plot==15){
-    params$CHOKE_GROUPS <- T;print("Plotting CHOKE_GROUPS")
-  }
-  
-  if(type2plot==16){
-    params$AverageQuota_EachFleet <- T; params$YLAB<-"t/km2";print("Plotting AverageQuota_EachFleet")
-  }
-  
-  ###F Trajectories
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
-  if(any(params$MORT_REAL_F,params$MORT_REAL_LandF,params$MORT_REAL_DiscF,params$MORT_HCRF_Cons,params$MORT_HCRF_Targ,params$QUOTA_HCRF_Cons,params$QUOTA_HCRF_Targ,params$CATCH,params$DISCARD,params$LANDING)) plot_fishing_trajectories(params)
-  
-  ###Biomass trajectoreis
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
-  if (params$BIOMASS) plot_biomass_trajectories(params)
   
   ###Value trajectories
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
-  if (params$VALUE) plot_value_trajectories(params)
-
-  ###Effort trajectories
+  if(type2plot==12){
+    params$VALUE <- T;print("Plotting VALUE")
+    plot_value_trajectories(params)
+  }
+  
+  ###Biomass trajectoreis
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
-  if (params$EFFORT) plot_effort_trajectories(params)
-
+  if(type2plot==13){
+    params$BIOMASS <-T;print("Plotting BIOMASS")
+    plot_biomass_trajectories(params)
+  }
+  
   ###HIGHEST_VALUE Pie chart
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  if(params$HIGHEST_VALUE) plot_highestvalue_pies(params)
+  if(type2plot==14){
+    params$HIGHEST_VALUE <- T;print("Plotting HIGHEST_VALUE")
+    plot_highestvalue_pies(params)
+  }
   
   ###CHOKE SPECIES Pie chart
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  if(params$CHOKE_GROUPS) plot_choke_pies(params)
-
+  if(type2plot==15){
+    params$CHOKE_GROUPS <- T;print("Plotting CHOKE_GROUPS")
+    if(params$CHOKE_GROUPS) plot_choke_pies(params)
+  }
+  
   ###Plot average target quota across a regulation type and models
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  if(params$AverageQuota_EachFleet) Plot_Average_Quotas(Path = params$RootPath, Groups = params$Groups2Plot, 
-                                                        Fleets = params$Fleets2Plot, TimeSteps =c(1:20), 
-                                                        RegulationTypes = c("Highest value", "Weakest stock", "Selective"))
+  if(type2plot==16){
+    params$AverageQuota_EachFleet <- T; params$YLAB<-"t/km2";print("Plotting AverageQuota_EachFleet")
+    Plot_Average_Quotas(Path = params$RootPath, Groups = params$Groups2Plot, 
+                        Fleets = params$Fleets2Plot, TimeSteps =c(1:20), 
+                        RegulationTypes = c("Highest value", "Weakest stock", "Selective"))
+    }
+  
 }
 
-for(iplot in c(13)){
+for(iplot in c(11:12)){
   print(paste("Currently plotting type", iplot))
   plot_type(iplot)
 }
