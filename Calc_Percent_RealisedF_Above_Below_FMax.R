c47c6f08125c4365102ce4c06c6ca4911513968b
MarkPlatts <mdplatts@yahoo.co.uk>
2017-03-06 15:48:10 +0000

    fixed bug - it was loading hcrquota_targ instead of realisedF

--- Calc_Percent_RealisedF_Above_Below_FMax.R
+++ Calc_Percent_RealisedF_Above_Below_FMax.R
@@ -15,6 +15,8 @@ hcr.folders = c("C:/Users/Mark/Dropbox/GAP2_MSE Plugin2/NorthSea Model/2015 FINA
                 "C:/Users/Mark/Dropbox/GAP2_MSE Plugin2/NorthSea Model/2015 FINAL Key Run/DATA/HCRs/Type3_BmsytoZeroatBlim",
                 "C:/Users/Mark/Dropbox/GAP2_MSE Plugin2/NorthSea Model/2015 FINAL Key Run/DATA/HCRs/Type4_BmsyBlimClifftoFmin")
 
+groups.for.f.or.biomass = "f"
+
 # INITIALISATION END ===============================================================================================
 
 
@@ -22,7 +24,7 @@ hcr.folders = c("C:/Users/Mark/Dropbox/GAP2_MSE Plugin2/NorthSea Model/2015 FINA
 
 if(TRUE){
   #get a list of all the groups
-  unique.groups = groupsWithHcr(hcr.folders)
+  unique.groups = groupsWithHcr(hcr.folders, groups.for.f.or.biomass)
   
   #get a list of strategy.tables with strategy name
   strategies.table = getStrategyTable(hcr.folders)
@@ -35,21 +37,22 @@ if(TRUE){
   
   for(igroup in unique.groups)
   {
-    
+
     #get the file in HCRQuota_Targ folder that are for "AllFleets"
-    realisedF.file = GetFileName_ContainsStrings(FolderPath = paste(root.results, "/HCRQuota_Targ/", sep=""), 
+    realisedF.file = GetFileName_ContainsStrings(FolderPath = paste(root.results, "/RealisedF/", sep=""), 
                                                  Strings = c(igroup), WithPath=T)
     #get a list of all the files in CatchTrajectories that are for "AllFleets"
     # catches.file = GetFileName_ContainsStrings(FolderPath = paste(root.results, "/CatchTrajectories/", sep=""), 
     #                                            Strings = c("AllFleets", igroup), WithPath=T)
     #get the hcr
     
+    realised.f = fread(realisedF.file, skip=7, header=T)
+    
     #Determine file is valid
-    # if(!isNotAllZero(file.name.with.path = hcr.quota.targ.file, col.data.starts = 6)) next
-    # if(!isNotAllZero(file.name.with.path = catches.file, col.data.starts = 6)) next
+    if(!isNotAll(dt = realised.f, col.data.starts = 4, val.to.check = -9999)) next
     
     #load the files and sum
-    realised.f = calcLast5Year(realisedF.file, "realised.f.last5yearsum", 5, function.type = 2)
+    realised.f = calcLast5Year(realised.f, "realised.f.last5yearsum", 5, function.type = 2)
     
     #add a column with the group name - need this to merge with the strategy data.table
     realised.f = appendVariableToDataTable(dt=realised.f, variable=igroup, variablename="GroupNameForF", beg=TRUE, end=FALSE)
@@ -57,7 +60,7 @@ if(TRUE){
     #merge the two together so that we can easily calculate the difference between two columns
     dt = merge(x=realised.f, y=strategies.table, by = c("StrategyName", "GroupNameForF"), all.x = TRUE)
     dt = cbind(dt,data.table(realf.maxf.diff = dt[,realised.f.last5yearsum] - dt[,MaxF]))
-    
+
     #Check where the catch is greater than or less than the quota
     dt[, "RealisedF.greater.than.maxF"] = dt$realf.maxf.diff>=0
     dt[, "RealisedF.less.than.maxF"] = dt$realf.maxf.diff<0
