82c7bce7a1b82add95473f463617ad80d7c0a501
MarkPlatts <mdplatts@yahoo.co.uk>
2017-03-14 11:20:24 +0000

    Made fix so that groupname column is the same in the strategies table biomass dt

--- Calc_Percent_Below_Conservation_Limits.R
+++ Calc_Percent_Below_Conservation_Limits.R
@@ -8,6 +8,7 @@ rm(list = ls())
 # setwd(source.folder.location)
 setwd("C:/Users/Mark/Desktop/MSE_Plugin_Results_Plotting")
 source("share_tools.R")
+#library(reshape)
 
 #root results path
 root.plot =     "C:/Users/Mark/Dropbox/GAP2_MSE Plugin2/North Sea MultiAnnual Plan/ResultsType1-4_220117/Plots/"
@@ -32,11 +33,11 @@ CreatePercentBelowConservationLimits = function(BLimitType){
   #get a list of hcrs by listed by strategy and group name with only biomass limits
   strategies.table = getStrategyTable(hcr.folders)
   strategies.table = filter(strategies.table, Target_or_Conservation==1)
-  strategies.table = strategies.table[,.(StrategyName, GroupNameForBiomass, Limit = get(BLimitType))]#, StepBiomass, UpperLimit)]
+  strategies.table = strategies.table[,.(StrategyName, GroupName = GroupNameForBiomass, Limit = get(BLimitType))]#, StepBiomass, UpperLimit)]
   strategies.table = unique(strategies.table)
-  
+
   #get a list of all the groups
-  unique.groups = unique(strategies.table$GroupNameForBiomass)
+  unique.groups = unique(strategies.table$GroupName)
 
   #Create a data.table in which to compile all the tables from each file ready for saving to csv
   dt.all = data.table()
@@ -51,14 +52,17 @@ CreatePercentBelowConservationLimits = function(BLimitType){
     #Load the file
     biomass = fread(biomass.file, skip=7, header=T)
     
+    #Determine file is valid
+    if(!isNotAll(dt = biomass, col.data.starts = 4, val.to.check = -9999)) next
+    
     #Sum last 5 year
     biomass = calcLast5Year(biomass, "biomass.last5yearsum", 4, function.type = 2)
 
     #add a column with the group name - need this to merge with the strategy data.table
-    biomass = appendVariableToDataTable(dt=biomass, variable=igroup, variablename="GroupNameForBiomass", beg=TRUE, end=FALSE)
+    biomass = appendVariableToDataTable(dt=biomass, variable=igroup, variablename="GroupName", beg=TRUE, end=FALSE)
 
     #merge the two together so that we can easily calculate the difference between two columns
-    dt = merge(x=biomass, y=strategies.table, by = c("StrategyName", "GroupNameForBiomass"), all.x = TRUE)
+    dt = merge(x=biomass, y=strategies.table, by = c("StrategyName", "GroupName"), all.x = TRUE)
 
     dt = dt[Limit!="NA"]
     dt = cbind(dt, data.table(avbiomass.hcr.limit.diff = dt[,biomass.last5yearsum] - dt[,Limit]))
@@ -79,6 +83,11 @@ CreatePercentBelowConservationLimits = function(BLimitType){
     #add column with groupname to differentiate after binding
     dt.counts.temp = cbind(data.table(GroupName = rep(igroup, dim(dt.counts.temp)[1])), dt.counts.temp) 
     
+    #join table to strategy table so we can output the limit
+    dt.counts.temp = merge(dt.counts.temp, strategies.table[,c("StrategyName", "GroupName", "Limit")], by=c("GroupName", "StrategyName"))
+
+    dt.counts.temp = rename(dt.counts.temp, c("Limit" = BLimitType))
+
     #bind them all together ready to be saved to csv
     dt.all = rbind(dt.all, dt.counts.temp)
     
