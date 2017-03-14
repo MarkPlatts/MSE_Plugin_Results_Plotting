b8caa7d82b92c9f0c2e9388a9936a795239c1713
MarkPlatts <mdplatts@yahoo.co.uk>
2017-03-14 11:18:43 +0000

    Made batch script more succint, readable and easy to configure

--- Strategy_Batch_Run.R
+++ Strategy_Batch_Run.R
@@ -1,17 +1,10 @@
-setwd("C:/Users/Mark/Desktop/MSE_Plugin_Results_Plotting")
-batch = "1"
 
-source("plotting 15July_2016_ChokeHighestValue_byFleet - Refactor.R")
+#Change elements in this vector to choose which batches to plot
+batches = c(3)
 
-setwd("C:/Users/Mark/Desktop/MSE_Plugin_Results_Plotting")
-batch = "2"
-source("plotting 15July_2016_ChokeHighestValue_byFleet - Refactor.R")
-
-
-
-for(iBatch in 1:3){
+for(iBatch in batches){
   setwd("C:/Users/Mark/Desktop/MSE_Plugin_Results_Plotting")
-  batch = iBatch
+  batch = toString(iBatch)
   cat("Batch:", batch)
   source("plotting 15July_2016_ChokeHighestValue_byFleet - Refactor.R")
 }
